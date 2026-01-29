6cf87e99374f597bd1c5472ab0ba92641e92f4f3
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-29 22:36:08 +0100

    python314Packages.renault-api: migrate to finalAttrs

--- pkgs/development/python-modules/renault-api/default.nix
+++ pkgs/development/python-modules/renault-api/default.nix
@@ -17,7 +17,7 @@
   typeguard,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "renault-api";
   version = "0.5.3";
   pyproject = true;
@@ -25,7 +25,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "hacf-fr";
     repo = "renault-api";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-L5Ld6CyMapW2qX5YmExQzfWddQkQjSHgy7sz3OtL4TQ=";
   };
 
@@ -53,16 +53,16 @@ buildPythonPackage rec {
     syrupy
     typeguard
   ]
-  ++ lib.concatAttrValues optional-dependencies;
+  ++ lib.flatten (builtins.attrValues finalAttrs.passthru.optional-dependencies);
 
   pythonImportsCheck = [ "renault_api" ];
 
   meta = {
     description = "Python library to interact with the Renault API";
     homepage = "https://github.com/hacf-fr/renault-api";
-    changelog = "https://github.com/hacf-fr/renault-api/releases/tag/v${version}";
+    changelog = "https://github.com/hacf-fr/renault-api/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "renault-api";
   };
-}
+})
