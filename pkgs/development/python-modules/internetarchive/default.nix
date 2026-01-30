27df4de994207b9d62eb339f5be97351b50e383d
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-30 01:56:56 +0100

    python313Packages.internetarchive: migrate to finalAttrs

--- pkgs/development/python-modules/internetarchive/default.nix
+++ pkgs/development/python-modules/internetarchive/default.nix
@@ -12,7 +12,7 @@
   urllib3,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "internetarchive";
   version = "5.7.2";
   pyproject = true;
@@ -20,7 +20,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "jjjake";
     repo = "internetarchive";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-eBTUKJs3j8LmQJSIBKAlDOjUglLHrjwtGx5O9Wn5C8Y=";
   };
 
@@ -60,9 +60,9 @@ buildPythonPackage rec {
   meta = {
     description = "Python and Command-Line Interface to Archive.org";
     homepage = "https://github.com/jjjake/internetarchive";
-    changelog = "https://github.com/jjjake/internetarchive/blob/${src.tag}/HISTORY.rst";
+    changelog = "https://github.com/jjjake/internetarchive/blob/${finalAttrs.src.tag}/HISTORY.rst";
     license = lib.licenses.agpl3Plus;
     maintainers = with lib.maintainers; [ pyrox0 ];
     mainProgram = "ia";
   };
-}
+})
